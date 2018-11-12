e4fda43d1625f2f7eea9284d0927ef306c9c64c5
Sandy Maguire <sandy@sandymaguire.me>
2018-11-11 21:14:06 -0500

    glue and boxes

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,98 @@
+{-# LANGUAGE ScopedTypeVariables, MultiParamTypeClasses,OverloadedStrings #-}
+---------------------------------------------------------
+-- |
+-- Copyright   : (c) 2006-2013, alpheccar.org
+-- License     : BSD-style
+--
+-- Maintainer  : misc@NOSPAMalpheccar.org
+-- Stability   : experimental
+-- Portability : portable
+--
+-- Test
+---------------------------------------------------------
+
+
 module Main where
 
-import Lib
 
-main :: IO ()
-main = someFunc
+import System.IO.Unsafe
+import Graphics.PDF
+import Data.Maybe(fromJust)
+import Control.Monad.IO.Class
+
+import Debug.Trace
+
+vertical = 200.0
+margin = 10.0
+debugText = "hello world"
+debugFontSize = 10
+lightBlue= Rgb 0.6 0.6 1.0
+
+testFont="/usr/share/texmf-dist/fonts/type1/public/droid/DroidSans.pfb"
+afm="/usr/share/texmf-dist/fonts/afm/public/droid/DroidSans.afm"
+
+data MyParaStyles = DebugStyle AnyFont | Normal
+data MyVertStyles = NormalPara
+
+
+instance ComparableStyle MyParaStyles where
+  isSameStyleAs (DebugStyle fa) (DebugStyle fb) = fa == fb
+  isSameStyleAs Normal Normal = True
+  isSameStyleAs _ _ = False
+
+
+instance Style MyParaStyles where
+    textStyle (DebugStyle f) = TextStyle (PDFFont f debugFontSize) black black FillText 1.0 1.0 1.0 1.0
+    textStyle Normal = TextStyle (PDFFont timesRoman debugFontSize) black black FillText 1.0 1.0 1.0 1.0
+
+    sentenceStyle _ = Nothing
+
+    wordStyle _ = Just $ \r m d ->
+      case m of
+          DrawWord -> d >> setWidth 0.5
+          DrawGlue -> d >> setWidth 0.5
+
+    updateStyle a = a
+
+instance ComparableStyle MyVertStyles where
+    isSameStyleAs NormalPara NormalPara = True
+
+instance ParagraphStyle MyVertStyles MyParaStyles  where
+
+
+testAll ::  PDFFont  -> PDF ()
+testAll theFont@(PDFFont f s) = do
+    page1 <- addPage Nothing
+    drawWithPage page1 $ do
+      displayFormattedText (Rectangle (10 :+ 0) (100 :+ 380)) NormalPara Normal $ do
+        setJustification FullJustification
+        paragraph $ do
+              txt $ "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor "
+              txt $ "incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud "
+              txt $ "exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute "
+              txt $ "irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla "
+              txt $ "pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia "
+              txt $ "deserunt mollit anim id est laborum."
+      displayFormattedText (Rectangle (150 :+ 0) (240 :+ 380)) NormalPara Normal $ do
+        setJustification FullJustification
+        paragraph $ do
+              txt $ "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor "
+              txt $ "incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud "
+              txt $ "exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute "
+              txt $ "irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla "
+              txt $ "pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia "
+              txt $ "deserunt mollit anim id est laborum."
+
+
+Just timesRoman = unsafePerformIO $ mkStdFont Times_Roman
+
+main :: IO()
+main = do
+    fontData <- readType1Font testFont afm
+    let rect = PDFRect 0 0 600 400
+    runPdf "demo.pdf" (standardDocInfo { author= "alpheccar éèçàü", compressed = False}) rect $ do
+        testFont <- mkType1Font fontData
+        testAll (PDFFont testFont debugFontSize)
+        traceM . show $ (spaceGlyph testFont)
+        traceM . show $ 1000 * (glyphWidth testFont debugFontSize $ spaceGlyph testFont) / fromIntegral debugFontSize
+
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,168 @@
-module Lib
-    ( someFunc
-    ) where
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE DeriveFoldable     #-}
+{-# LANGUAGE DeriveFunctor      #-}
+{-# LANGUAGE DeriveGeneric      #-}
+{-# LANGUAGE DeriveTraversable  #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE TupleSections      #-}
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+module Lib where
+
+import Data.Maybe (fromJust)
+import Data.Bool
+import Control.Applicative
+import Control.Monad
+import Data.Data
+import Data.Foldable
+import Data.Monoid
+import GHC.Generics
+
+
+data Block
+  = Heading Int Inline
+  | Plain [Inline]
+  | Paragraph [Inline]
+
+data Inline
+  = Str String
+  | HardBreak
+  | Space
+
+-- data Layout
+--   = Box Point Size
+--   | Anchor Side Dimension
+
+-- data Flow
+--    = Flow [Layout]
+
+data Side
+  = TopSide
+  | LeftSide
+  | RightSide
+  | BottomSide
+
+data Document
+
+data Physical
+
+-- append semantics?
+instance Monoid Document where
+  mempty  = undefined
+  mappend = undefined
+
+type Point = (Int, Int)
+type Size = (Dimension, Dimension)
+type Dimension = Int
+
+-- flow :: Block -> Flow -> Document
+-- flow = undefined
+
+overlay :: Document -> Document -> Document
+overlay = undefined
+
+-- ???
+paginate :: Document -> Size -> Physical
+paginate = undefined
+
+
+data Glue x = Glue
+  { glueShrink  :: Float
+  , glueSize    :: Float
+  , glueStretch :: Float
+  }
+  deriving (Generic, Typeable, Data, Show)
+
+instance Monoid (Glue x) where
+  mempty = Glue 0 0 0
+  mappend (Glue a1 a2 a3)
+          (Glue b1 b2 b3) = Glue (a1 + b1) (a2 + b2) (a3 + b3)
+
+data Box = Box
+  { boxWidth  :: Float
+  , boxHeight :: Float
+  }
+  deriving (Generic, Typeable, Data, Show)
+
+data Horizontal
+
+type HBox = Either Box (Glue Horizontal)
+
+bakeH :: Traversable t => t HBox -> [HBox]
+bakeH = bake . toList
+  where
+    bake [] = []
+    bake (Left b1 : Left b2 : xs) =
+      bake $ (Left $ Box (boxWidth b1 + boxWidth b2) $ max (boxHeight b1) (boxHeight b2)) : xs
+    bake (Right g1 : Right g2 : xs) =
+      bake $ (Right $ g1 <> g2) : xs
+    bake (x : xs) = x : bake xs
+
+getHSize :: HBox -> Glue Horizontal
+getHSize (Left (Box w _)) = Glue 0 w 0
+getHSize (Right g)        = g
+
+getSize :: Foldable t => t HBox -> Glue Horizontal
+getSize = foldMap getHSize
+
+data Region = Region
+  { regWidth  :: Float
+  , regHeight :: Float
+  }
+  deriving (Generic, Typeable, Data)
+
+makeBox :: Char -> HBox
+makeBox ' ' = Right $ Glue 0.5 1 0.5
+makeBox c   = Left  $ Box 1 2
+
+boxItUp :: String -> [HBox]
+boxItUp = bakeH . fmap makeBox
+
+minSize :: Glue x -> Float
+minSize = liftA2 (-) glueSize glueShrink
+
+maxSize :: Glue x -> Float
+maxSize = liftA2 (+) glueSize glueStretch
+
+data Interval a = Interval
+  { iMin :: a
+  , iMax :: a
+  }
+  deriving (Eq, Ord, Show, Functor, Foldable, Traversable)
+
+stretchability :: [Glue x] -> Interval Float
+stretchability g =
+  let Glue l v h = mconcat g
+   in Interval (v - l) (v + h)
+
+type Badness = Float
+
+-- stretch' :: Float -> (Glue x -> Float) -> Glue x -> Maybe (Glue x)
+-- stretch' sr f g@(Glue l w h) = do
+--   guard $ sr >= -1
+--   guard $ sr <= 1
+--   let s = f g * sr
+--   pure $ Glue (l + s) (w + s) (h - s)
+
+stretch :: Float -> Glue x -> Maybe (Glue x)
+stretch sr (Glue l w h) = do
+  guard $ sr >= -1
+  guard $ sr <= 1
+  let s = bool l h (sr > 0) * sr
+  pure $ Glue (l + s) (w + s) (h - s)
+
+setHGlue
+    :: Float  -- ^ desired width
+    -> [HBox]
+    -> Maybe ([HBox], Badness)
+setHGlue desired hbox = do
+  let together = getSize hbox
+      size     = glueSize together
+      delta    = desired - size
+  guard $ minSize together <= desired
+       && desired <= maxSize together
+  pure $ (, delta ^ 2) $ case signum delta of
+    0  -> hbox
+    1  -> fmap (fmap $ fromJust . stretch (delta / glueStretch together)) hbox
+    -1 -> fmap (fmap $ fromJust . stretch (delta / glueShrink together)) hbox
+
+
